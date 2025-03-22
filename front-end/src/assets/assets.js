3b0be357f983b00e402c944ea1c4f7c114395a8d
dthangk4 <dqthang17@gmail.com>
2025-03-22 16:44:27 +0700

    Upload

--- front-end/src/assets/assets.js
+++ front-end/src/assets/assets.js
@@ -11,6 +11,18 @@ import ga_bo_xoi_3_mau from "./ga_bo_xoi_3_mau.jpg";
 import ga_hap_nuoc_mam from "./ga_hap_nuoc_mam.jpg";
 import ga_ta_hap_cach_thuy from "./ga_ta_hap _cach_thuy.jpg";
 import gio_heo_chien from "./gio_heo_chien.jpg";
+import heo_quay from "./heo_quay.jpg"
+import lau_ca_bop from "./lau_ca_bop.jpg"
+import lau_ga from "./lau_ga.jpg"
+import lau_hai_san from "./lau_hai_san.jpg"
+import lau_suon_be from "./lau_suon_be.jpg"
+import Logo from "./Logo.png"
+import sup_cua from "./sup_cua.jpg"
+import sup_hai_san from "./sup_hai_san.jpg"
+import tomg_rang_hongkong from "./tom_rang_hongkong.jpg"
+import tom_su_trung_muoi from "./tom_su_trung_muoi.jpg"
+
+
 
 export const menu_list = [
     {

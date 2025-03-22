13e52a11e44c15c4e3c89fd463100a20bde07ffa
mbyhphat <22521067@gm.uit.edu.vn>
2025-03-22 15:36:19 +0700

    Update

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import NavBar from './components/NavBar/NavBar'
-import {Route, Routes} from 'react-router-dom'
+import React from "react";
+import NavBar from "./components/NavBar/NavBar";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className='app'>
-      <NavBar/>
+    <div className="app">
+      <NavBar />
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/cart' element={<Cart/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

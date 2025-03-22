4e6ded8a141215ea84ee1206cef03f7b927c93cf
mbyhphat <22521067@gm.uit.edu.vn>
2025-03-22 16:41:24 +0700

    Create menu bar

---
+++ front-end/src/components/Explore Menu/ExploreMeu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExploreMeu = () => {
+  return (
+    <div className='explore-menu'>
+      <h1>Thực đơn</h1>      
+    </div>
+  )
+}
+
+export default ExploreMeu

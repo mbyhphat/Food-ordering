35c70590a532194b466c31c138137f229b46a12a
dthangk4 <dqthang17@gmail.com>
2025-03-22 19:03:37 +0700

    Update

--- front-end/src/components/NavBar/NavBar.jsx
+++ front-end/src/components/NavBar/NavBar.jsx
@@ -6,11 +6,13 @@ const NavBar = () => {
   return (
     <div className="navbar">
       <img src={assets.logo} alt="Logo" className="Logo" />
-      <ul className="navbar-menu"> </ul>
-      <li>Home </li>
+      <ul className="navbar-menu"> 
+      <li>Home  <i class="fa-solid fa-magnifying-glass"></i></li>
       <li>menu </li>
       <li>Mobile-app </li>
       <li>Contact us  </li>
+      </ul>
+      
       
     </div>
   )

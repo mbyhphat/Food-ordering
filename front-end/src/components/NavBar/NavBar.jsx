44801d1cfeac23e14cfbf2726ba6d63902c3f2d2
dthangk4 <dqthang17@gmail.com>
2025-03-24 08:59:39 +0700

    Update

--- front-end/src/components/NavBar/NavBar.jsx
+++ front-end/src/components/NavBar/NavBar.jsx
@@ -1,21 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import "./NavBar.css";
 import { assets } from "../../assets/assets";
 
 const NavBar = () => {
+
+  const [menu, Setmenu] = useState("Home"); 
+
   return (
     <div className="navbar">
-      <img src={assets.logo} alt="Logo" className="Logo" />
+      <img src={assets.logo} alt="Logo" className="logo" />
       <ul className="navbar-menu"> 
-      <li>Home </li>
-      <li>Menu </li>
-      <li>Mobile-app </li>
-      <li>Contact us  </li>
+      <li className={menu==="Home"?"active":""}>Home </li>
+      <li className={menu==="Menu"?"active":""}>Menu </li>
+      <li className={menu==="Mobile-app"?"active":""}>Mobile-app </li>
+      <li className={menu==="Contact us"?"active":""}>Contact us  </li>
       </ul>
       <div className="navbar-right">
         <i class="fa-solid fa-magnifying-glass"></i>
-        <i class="fa-solid fa-cart-shopping"></i>
-        <i class="fa-solid fa-user"></i>
+        <div className="navbar-search-icon">
+          <i class="fa-solid fa-cart-shopping"></i>
+          <div className="dot"></div>
+        </div>
+        <button>Đăng nhập</button>
       </div>
       
     </div>

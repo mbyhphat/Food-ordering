ad0dff529862a52502dcf89d4ec950f9ab27e2d7
mbyhphat <22521067@gm.uit.edu.vn>
2025-03-26 13:44:40 +0700

    Create menu category

--- front-end/src/components/Explore Menu/ExploreMenu.jsx
+++ front-end/src/components/Explore Menu/ExploreMenu.jsx
@@ -2,15 +2,27 @@ import React from "react";
 import { menu_list } from "../../assets/assets";
 import "./ExploreMenu.css";
 
-const ExploreMenu = () => {
+const ExploreMenu = ({ category, setCategory }) => {
   return (
     <div className="explore-menu">
       <h1>Thực đơn</h1>
       <div className="menu-category-list">
         {menu_list.map((item, index) => {
           return (
-            <div key={index} className="menu-category">
-              <img src={item.menu_image} alt="" />
+            <div
+              onClick={() =>
+                setCategory((prev) =>
+                  prev === item.menu_name ? "All" : item.menu_name
+                )
+              }
+              key={index}
+              className="menu-category"
+            >
+              <img
+                className={category === item.menu_name ? "active" : ""}
+                src={item.menu_image}
+                alt=""
+              />
               <p>{item.menu_name}</p>
             </div>
           );

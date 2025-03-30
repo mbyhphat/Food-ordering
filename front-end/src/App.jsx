6c298778d87ffe54562ce73a26029e3d73bc9c7c
mbyhphat <22521067@gm.uit.edu.vn>
2025-03-30 09:10:43 +0700

    Create route

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
+import ExploreMenu from "./components/Explore Menu/ExploreMenu";
+import FoodDisplay from "./components/FoodDisplay/FoodDisplay";
+import Home from './pages/Home/Home';
+import Menu from "./pages/Menu/Menu";
 
 const App = () => {
   return (
-    <div className="header">
-      <div className="app">
+    <div className="app">
       <NavBar />
-      {/* <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes> */}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/menu' element={<Menu />} />
+      </Routes>
+      {/* <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay /> */}
+    {/* <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cart" element={<Cart />} />
+    </Routes> */}
     </div>
-    </div>
+    
   );
 };
 

097ee023d229168138bd560adfbee3e04ef82100
mbyhphat <22521067@gm.uit.edu.vn>
2025-04-19 10:06:19 +0700

    Update project

--- food-ordering-app/front-end/src/components/FoodItem/FoodItem.jsx
+++ food-ordering-app/front-end/src/components/FoodItem/FoodItem.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import "./FoodItem.css";
 import { StoreContext } from "../../context/StoreContext";
 

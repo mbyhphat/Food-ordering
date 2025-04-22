2ab9f5b3801d77b8bad542b4a0d0421dfcd9c310
mbyhphat <22521067@gm.uit.edu.vn>
2025-04-22 16:26:36 +0700

    Implement login

--- project/app/Http/Controllers/AuthController.php
+++ project/app/Http/Controllers/AuthController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
@@ -22,6 +24,21 @@ class AuthController extends Controller
             'role' => 0,
         ]);
 
+        $token = $user->createToken('main')->plainTextToken;
+        return response('', 201);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->validated();
+        if (!Auth::attempt($credentials)) {
+            return response([
+                'message' => 'Địa chỉ email hoặc mật khẩu không chính xác!'
+            ], 422);
+        }
+
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
         $token = $user->createToken('main')->plainTextToken;
         return response(compact('user', 'token'));
     }

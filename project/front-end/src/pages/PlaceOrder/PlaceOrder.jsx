f5884b92f404415be307deb66a7b1e7b421e1555
dthangk4 <dqthang17@gmail.com>
2025-05-18 14:55:50 +0700

    Update

--- project/front-end/src/pages/PlaceOrder/PlaceOrder.jsx
+++ project/front-end/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -7,45 +7,57 @@ const PlaceOrder = () => {
     const { getTotalCartAmount } = useContext(StoreContext);
     const { user } = useAppContext();
 
-    const {getTotalCartAmount} = useContext(StoreContext)
-  return (
-    <form className="place-order">
-      <div className='place-order-left'>
-        <p className="title">
-          Thông tin giao hàng  
-        </p>
-        <div className="multi-fields">
-          <input type="text" placeholder='First name' />
-          <input type="text" placeholder='Last name' />
-        </div>
-        <input type="email" placeholder='Gmail' />
-        <input type="text" placeholder='Địa chỉ ' />
-        <input type="text" placeholder='Số điện thoại ' />
-      </div>
-      <div className='place-order-right'>
-        <div className="cart-total">
-          <h2>Giỏ hàng </h2>
-          <div>
-            <div className="cart-total-details">
-              <p>Tổng tiền món ăn  </p>
-              <p>{getTotalCartAmount()}</p>
+    return (
+        <form className="place-order">
+            <div className="place-order-left">
+                <p className="title">Thông tin giao hàng</p>
+                <div className="user-info">
+                    <div className="user-name">
+                        <p>Họ và tên:</p>
+                        <input type="text" value={user.name} />
+                    </div>
+                    <div className="user-gmail">
+                        <p>Gmail:</p>
+                        <input type="email" value={user.email} />
+                    </div>
+                    <div className="user-address">
+                        <p>Địa chỉ:</p>
+                        <input type="text" value={user.address} />
+                    </div>
+                    <div className="user-phone">
+                        <p>Số điện thoại:</p>
+                        <input type="text" value={user.phone} />
+                    </div>
+                </div>
             </div>
-            <hr />
-            <div className="cart-total-details">
-              <p>Phí giao hàng</p>
-              <p>{20000}</p>
+            <div className="place-order-right">
+                <div className="cart-total">
+                    <h2>Giỏ hàng </h2>
+                    <div>
+                        <div className="cart-total-details">
+                            <p>Tổng tiền món ăn </p>
+                            <p>{getTotalCartAmount()}</p>
+                        </div>
+                        <hr />
+                        <div className="cart-total-details">
+                            <p>Phí giao hàng</p>
+                            <p>{20000}</p>
+                        </div>
+                        <hr />
+                        <div className="cart-total-details">
+                            <p>Tổng tiền </p>
+                            <b>
+                                {getTotalCartAmount() === 0
+                                    ? 20000
+                                    : getTotalCartAmount() + 20000}
+                            </b>
+                        </div>
+                    </div>
+                    <button>Thanh toán </button>
+                </div>
             </div>
-            <hr />
-            <div className="cart-total-details">
-              <p>Tổng tiền </p>
-              <b>{getTotalCartAmount()===0?0:getTotalCartAmount()+20000}</b>
-            </div>
-          </div>
-          <button >Thanh toán </button>
-        </div>
-      </div>
-    </form>
-  )
-}
+        </form>
+    );
+};
 
 export default PlaceOrder;

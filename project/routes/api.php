67af9a0fee0fc53298c457e7b8a5f4ac33e3fc7a
mbyhphat <22521067@gm.uit.edu.vn>
2025-05-20 08:03:54 +0700

    config: connect admin

--- project/routes/api.php
+++ project/routes/api.php
@@ -4,14 +4,24 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\FoodController;
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::apiResource('users', UserController::class);
 });
+
+// Route::middleware(['auth:sanctum', 'check.role:1'])->group(function () {
+
+// });
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/category', [CategoryController::class, 'index']);
 Route::get('/food', [FoodController::class, 'index']);
+Route::apiResource('users', UserController::class);
+Route::apiResource('category', CategoryController::class);
+
+// Route::middleware(['check.role:1'])->get('/test-role', function () {
+//     return 'Middleware chạy rồi!';
+// });

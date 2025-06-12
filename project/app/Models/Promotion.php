d9cf31d30beb42af3eda91f19ef1e42a27a46870
mbyhphat <22521067@gm.uit.edu.vn>
2025-06-13 04:33:30 +0700

    fix: disable timestep

--- project/app/Models/Promotion.php
+++ project/app/Models/Promotion.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Promotion extends Model
 {
+    protected $table = 'promotions';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
     protected $fillable = [
         'code',
         'description',
